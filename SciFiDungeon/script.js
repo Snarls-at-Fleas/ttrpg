465b1657294b44696a4c33b4490419106569c45b
Snarls-at-Fleas <berlin.pavel@gmail.com>
2021-08-11 13:52:20 +0300

    Add (overambitious) plans for a dungeon exploration game

---
+++ SciFiDungeon/script.js
@@ -0,0 +1,11 @@
+// A game of dungeon exploration in sci-fi environment
+
+// Based on OSR sci-fi games such as Stars Without Number by Kevin Crawford
+
+// Planned features:
+// Random dungeon generation
+//      Rooms content (description, monsters, loot etc)
+//      Dungeon topology (corridors, exits, secret doors etc)
+// Round based combat
+// Character generation
+// Character advancement
