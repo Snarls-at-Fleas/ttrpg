aa6e20825680397f0b66e7a4d072eed5606634fa
Snarls-at-Fleas <berlin.pavel@gmail.com>
2021-08-31 09:00:56 +0300

    Removed CharGen & Edited the roadmap to be more realistic for now

--- SciFiDungeon/script.js
+++ SciFiDungeon/script.js
@@ -3,9 +3,15 @@
 // Based on OSR sci-fi games such as Stars Without Number by Kevin Crawford
 
 // Planned features:
+// -----------------
+// Character generation (START!!!)
+//      Random stats
+//      Choose class
+//      Choose foci
+//      Buy equipment
+//      Save to array of objects (to JSON when I learn to do it)
 // Random dungeon generation
 //      Rooms content (description, monsters, loot etc)
 //      Dungeon topology (corridors, exits, secret doors etc)
 // Round based combat
-// Character generation
 // Character advancement
